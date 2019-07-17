4536cbff684c1e9ecd150079af68097bc073491f
unknown <brandon.nguyen@ni.com>
2019-07-16 21:59:55 -0500

    clone build tools

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -1,19 +1,13 @@
 def call(lvVersion) {
 	def diffDir = "${WORKSPACE}\\diff_dir"
-	def stepsDir = "${WORKSPACE}\\steps"
+	def stepsDir = "${WORKSPACE}\\jenkinsbuildsystem\\steps"
 	bat "if exist ${diffDir} rmdir /s /q ${diffDir}"
 	bat "mkdir ${diffDir}"
 
 	echo 'Running LabVIEW diff build between origin/master and this commit'
+
+	bat "git difftool --no-prompt --extcmd=\"${WORKSPACE}\\jenkinsbuildsystem\\steps\\labview-diff.bat\" \$LOCAL \$REMOTE diff_dir ${lvVersion}\" origin/master HEAD"
 	
-	
-	
-	/*
-	def diffDir = "${WORKSPACE}\\diff_dir"
-	bat "if exist ${diffDir} rd /s /q ${diffDir}"
-	bat "mkdir ${diffDir}"
-	bat "git difftool --no-prompt --extcmd=\"'L:\\labview-diff.bat' \$LOCAL \$REMOTE diff_dir ${lvVersion}\" origin/master HEAD"
-	*/
 	// Silencing echo so as to not print out the token.
 	//bat "@python L:\\github_commenter.py --token=${GITHUB_DIFF_TOKEN} --pic-dir=${diffDir} --pull-req=${CHANGE_ID} --info=${JOB_NAME} --pic-repo=${DIFFING_PIC_REPO}"
 }
