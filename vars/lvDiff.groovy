101e8b99243fb02a782cb159519b7033f95e5cc4
unknown <brandon.nguyen@ni.com>
2019-07-17 00:15:05 -0500

    Added python commenter string

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -3,13 +3,13 @@ def call(lvVersion, ORG_NAME) {
 	def stepsDir = "${WORKSPACE}\\jenkinsbuildsystem\\steps"
 	def prNum = env.CHANGE_ID
 	def repo = getComponentParts()['repo']
-	def gitHubdifftoken = "41940eef93d0080aea72" + fa004f02551bc5b56174
+	def gitHubdifftoken = "41940eef93d0080aea72" + "fa004f02551bc5b56174"
 
 	echo 'Running LabVIEW diff build between origin/master and this commit'
 
 	bat "python -u \"${stepsDir}\\labview_diff.py\" \"${WORKSPACE}\" \"${diffDir}\" ${lvVersion} --target=origin/master"
     
-	//bat "python -u \"${stepsDir}\\github_commenter.py\" -t "${gitHubdifftoken}" -d \"${diffDir}\" -p env.CHANGE_ID i- \"ORG_NAME/${repo}/${env.CHANGE_ID}\" -r \"ORG_NAME/${repo}\""
+	bat "python -u \"${stepsDir}\\github_commenter.py\" -t "${gitHubdifftoken}" -d \"${diffDir}\" -p env.CHANGE_ID i- \"ORG_NAME/${repo}/${env.CHANGE_ID}\" -r \"ORG_NAME/${repo}\""
 	
 	//bat "@python -u \"${resourcesDir}/github_commenter.py\" --token="${githubDiffToken}" --pic-dir=\"${diffDir}\" --pull-req=\"${CHANGE_ID}\" --info=\"${JOB_NAME}\" --pic-repo=\"${diffingPicRepo}\"
 	
