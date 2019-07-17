3dce335edbeacdddeed1a5241fcdaab66380abe1
unknown <brandon.nguyen@ni.com>
2019-07-17 12:11:31 -0500

    no comment

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -9,5 +9,5 @@ def call(lvVersion, ORG_NAME) {
 
 	bat "python -u \"${stepsDir}\\labview_diff.py\" \"${WORKSPACE}\" \"${diffDir}\" ${lvVersion} --target=origin/master"
     
-	bat "python -u \"${stepsDir}\\github_commenter.py\" --token=\"${gitHubdifftoken}\" --pic-dir=\"${diffDir}\" --pull-req=\"${env.CHANGE_ID}\" --info=\"${ORG_NAME}/${repo}/${env.CHANGE_ID}\" --pic-repo=\"${ORG_NAME}/${repo}\""
+	//bat "python -u \"${stepsDir}\\github_commenter.py\" --token=\"${gitHubdifftoken}\" --pic-dir=\"${diffDir}\" --pull-req=\"${env.CHANGE_ID}\" --info=\"${ORG_NAME}/${repo}/${env.CHANGE_ID}\" --pic-repo=\"${ORG_NAME}/${repo}\""
 }
