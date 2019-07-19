058c062c8e4c074ba4c0c71fb045dfb42b96275d
unknown <brandon.nguyen@ni.com>
2019-07-18 23:29:11 -0500

    Moved build system repo to env var

--- vars/cloneBuildTools.groovy
+++ vars/cloneBuildTools.groovy
@@ -2,6 +2,6 @@ def call(){
    echo 'Cloning build tools to workspace.'
    
    def branch = env."library.vs-build-tools.version"
-   buildToolsDir = cloneRepo("https://github.com/${BUILD_SYSTEM_REPO}", branch)
+   buildToolsDir = cloneRepo("https://github.com/${ORG_NAME}/${BUILD_SYSTEM_REPO}", branch)
    return buildToolsDir
 }
