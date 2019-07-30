51e728c3055a46fc71512c742d3ea607560fcf2a
unknown <brandon.nguyen@ni.com>
2019-07-30 12:15:55 -0500

    added file moving to archive

--- vars/postClean.groovy
+++ vars/postClean.groovy
@@ -1,6 +1,7 @@
 def call() {
+	def stepsDir = "${WORKSPACE}\\${BUILD_SYSTEM_REPO}\\steps"
+	
 	echo 'Archiving all artifacts'
-	echo '${LV_BUILD_OUTPUT_DIR}'
 	bat "Python -u \"${stepsDir}\\move_files.py\" \"${LV_BUILD_OUTPUT_DIR}\" \"${WORKSPACE}\\TEMPDIR\""
 	archiveArtifacts artifacts: 'TEMPDIR/**'
 	echo 'Archive complete!'
